49efd46a966190fb630e2dead2864a445582026b
kristian <kristian.tan@ntlworld.com>
2019-04-14 17:16:27 +0100

    Added class to hold user height, age and weight

---
+++ app/src/main/java/durm/caloriecounter/CalculateCaloricIntake.java
@@ -0,0 +1,9 @@
+package durm.caloriecounter;
+
+public class CalculateCaloricIntake {
+    // Get variables from user input screen
+    private double weight;
+    private double height;
+    private int age;
+    
+}
