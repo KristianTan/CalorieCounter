36d1aec3032965cd51101708e4a353f394eddc75
kristian <kristian.tan@ntlworld.com>
2019-04-14 17:37:12 +0100

    Created model to hold data about the user

---
+++ app/src/main/java/durm/caloriecounter/User.java
@@ -0,0 +1,50 @@
+package durm.caloriecounter;
+
+public class User {
+    // Get variables from user input screen
+    private double weight;
+    private double height;
+    private int age;
+    private enumGender gender;
+    private enumGoal goal;
+
+    public enumGoal getGoal() {
+        return goal;
+    }
+
+    public void setGoal(enumGoal goal) {
+        this.goal = goal;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+
+    public void setWeight(double weight) {
+        this.weight = weight;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public void setHeight(double height) {
+        this.height = height;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public enumGender getGender() {
+        return gender;
+    }
+
+    public void setGender(enumGender gender) {
+        this.gender = gender;
+    }
+}
