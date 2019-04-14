4f74b9ae6dacbf7bc48a26d39bc3d413f4c4969a
kristian <kristian.tan@ntlworld.com>
2019-04-14 17:27:56 +0100

    Provided logic to calculate caloric intake using Mifflin-St Jeor equation

--- app/src/main/java/durm/caloriecounter/CalculateCaloricIntake.java
+++ app/src/main/java/durm/caloriecounter/CalculateCaloricIntake.java
@@ -5,5 +5,16 @@ public class CalculateCaloricIntake {
     private double weight;
     private double height;
     private int age;
-    
+    private enumGender gender;
+
+    // Convert to kg and cm
+
+    public double calculateCalories() {
+        // Use the Mifflin-St Jeor equation to calculate caloric intake
+
+        int s = this.gender == enumGender.MALE ? 5 : -161; // s variable is a constant for this equation
+
+        return 10 * this.weight + 6.25 * this.height - 5 * this.age + s;
+    }
+
 }
