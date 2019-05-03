a9427755103aa80d6af4080be1a076f18400b1a9
kristian.tan <kristian.tan@cs2-20.cs>
2019-05-03 11:46:51 +0100

    Added listener for searchView

--- app/src/main/java/durm/caloriecounter/activities/SearchActivity.java
+++ app/src/main/java/durm/caloriecounter/activities/SearchActivity.java
@@ -2,19 +2,34 @@ package durm.caloriecounter.activities;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.SearchView;
 
 import durm.caloriecounter.R;
 
 public class SearchActivity extends AppCompatActivity {
 
     // This activity is launched from the fab button.
+    SearchView searchView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
 
+        searchView = findViewById(R.id.searchBar);
+        searchView.setSubmitButtonEnabled(true);
 
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) { return false; }
+        });
 
     }
+
 }
