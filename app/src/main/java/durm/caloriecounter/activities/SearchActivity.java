797c5602e19d644cadc2adea5524164c039df671
kristian <kristian.tan@ntlworld.com>
2019-05-05 13:55:29 +0100

    Search results are displayed but can't be opened

--- app/src/main/java/durm/caloriecounter/activities/SearchActivity.java
+++ app/src/main/java/durm/caloriecounter/activities/SearchActivity.java
@@ -3,29 +3,50 @@ package durm.caloriecounter.activities;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.View;
+import android.widget.LinearLayout;
 import android.widget.SearchView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
 import durm.caloriecounter.R;
 import durm.caloriecounter.fragments.Recipe_Item_Data_Fragment;
 import durm.caloriecounter.models.Recipe;
+import durm.caloriecounter.models.RecipeListSingleton;
 import durm.caloriecounter.requests.GetRecipeData;
 import durm.caloriecounter.requests.GetSearchResults;
+import durm.caloriecounter.viewAdapters.SearchResultsViewAdapter;
+import durm.caloriecounter.viewAdapters.ViewAdapter;
 
 public class SearchActivity extends AppCompatActivity {
-
+    final public static ArrayList<String> titles = new ArrayList<>();
+    final public static ArrayList<String> info = new ArrayList<>();
     // This activity is launched from the fab button.
-    SearchView searchView;
+    private SearchView searchView;
+    private static SearchResultsViewAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
+        titles.clear();
+        info.clear();
+        RecyclerView recyclerView = findViewById(R.id.RecyclerViewSearchResults);
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(linearLayoutManager);
+
+        adapter = new SearchResultsViewAdapter(this, titles, info);
+
+        recyclerView.setHasFixedSize(false);
+        recyclerView.setAdapter(adapter);
 
         searchView = findViewById(R.id.searchBar);
         searchView.setSubmitButtonEnabled(true);
+
 
         searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
             @Override
@@ -35,6 +56,21 @@ public class SearchActivity extends AppCompatActivity {
                      @Override
                      public void processFinish(ArrayList<Recipe> output) {
                         // Display list of recipes
+                         titles.clear();
+                         info.clear();
+                         RecipeListSingleton.getInstance().searchResultsList.clear();
+
+                         if (output.size() != 0) {
+                             for (Recipe r : output) {
+                                 titles.add(r.getLabel());
+                                 info.add(r.getCalories() + " cal");
+                                 adapter.notifyDataSetChanged();
+                                 RecipeListSingleton.getInstance().searchResultsList.add(r);
+                             }
+                         } else {
+                             Toast savedToast = Toast.makeText(getApplicationContext(), "No results found for: '" + query + "'", Toast.LENGTH_SHORT);
+                             savedToast.show();
+                         }
                      }
                  }).execute(query);
 
--- app/src/main/java/durm/caloriecounter/viewHolders/SearchResultsViewHolder.java
+++ app/src/main/java/durm/caloriecounter/viewHolders/SearchResultsViewHolder.java
@@ -7,21 +7,20 @@ import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import durm.caloriecounter.activities.MainActivity;
 import durm.caloriecounter.R;
-import durm.caloriecounter.fragments.Menu_Item_Data_Fragment;
+import durm.caloriecounter.activities.MainActivity;
 import durm.caloriecounter.models.Recipe;
 import durm.caloriecounter.models.RecipeListSingleton;
 
 
-public class FoodMenuViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
+public class SearchResultsViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
 
 
 
     public TextView titleText;
     public TextView infoData;
 
-    public FoodMenuViewHolder(@NonNull View itemView) {
+    public SearchResultsViewHolder(@NonNull View itemView) {
         super(itemView);
 
         // Set the data responsable for meals.
@@ -44,19 +43,19 @@ public class FoodMenuViewHolder extends RecyclerView.ViewHolder implements View.
         // Show that fragment.
 
         activity.getSupportFragmentManager().beginTransaction().setCustomAnimations(android.R.anim.fade_in,android.R.anim.fade_out,android.R.anim.fade_in,android.R.anim.fade_out)
-                .show(MainActivity.itemDataFragment).hide(MainActivity.fragment1).addToBackStack(null).commit();
+                .show(MainActivity.itemDataFragment).hide(MainActivity.fragment2).addToBackStack(null).commit();
 
 //        activity.getSupportFragmentManager().beginTransaction().setCustomAnimations(R.anim.enter_from_right,R.anim.exit_to_left,R.anim.enter_from_left,R.anim.exit_to_right)
-//              .show(MainActivity.itemDataFragment).hide(MainActivity.fragment2).addToBackStack(null).commit();
+//              .show(MainActivity.itemDataFragment).hide(MainActivity.fragment1).addToBackStack(null).commit();
 
 
 
         // Tell the code on what fragment we are so we can access it anywhere.
-        MainActivity.foodActiveFragment = getAdapterPosition();
+//        MainActivity.foodActiveFragment = getAdapterPosition();
 
         String ingredients = "";
 
-        Recipe thisRecipe = RecipeListSingleton.getInstance().recipeList.get(getAdapterPosition());
+        Recipe thisRecipe = RecipeListSingleton.getInstance().searchResultsList.get(getAdapterPosition());
 
         int i =1;
         for(String item : thisRecipe.getIngredients()) {
@@ -70,7 +69,7 @@ public class FoodMenuViewHolder extends RecyclerView.ViewHolder implements View.
         MainActivity.itemDataFragment.getRecipeName().setText(thisRecipe.getLabel());
 
 //        MainActivity.itemDataFragment.getHowToMakeData().setText(MainActivity.itemDataFragment.getHowToMake());
-//        Toast.makeText(activity,getAdapterPosition()+"",Toast.LENGTH_SHORT).show();
+        Toast.makeText(activity,getAdapterPosition()+"", Toast.LENGTH_SHORT).show();
 
     }
 
